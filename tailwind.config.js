9b2196384dbd663d65ae911e8cca5ccff0763fab
Julyant Anggara <julyantanggara02@gmail.com>
2024-03-23 12:10:49 +0700

    tugas2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
-
-}
+};
